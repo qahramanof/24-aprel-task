73180d2873376a048957e6f008ea82d402678d18
unknown <kananng@code.edu.az>
2023-04-24 17:37:13 +0400

    created

---
+++ aprel 24 task/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using aprel_24_task;
+
+Student stu1 = new("aqsin","hummatov","p237",85);
+
+Student stu2 = new("Kenan", "Gehremanov", "P137", 75);
+
+Student stu3 = new("Ebdul", "Sakirov", "p123", 79);
+
+Console.WriteLine(stu1.Info());
+
+
+
+if (stu3.IsGraduated() == true)
+{
+    Console.WriteLine(" 2-ci Imtahana  kecid ede bilersiz");
+}
+else
+{
+    Console.WriteLine("Imtaana kecid ede bilmersiniz");
+}
+
+Console.WriteLine(stu3.IsGraduated());
---
+++ aprel 24 task/student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aprel_24_task;
+
+internal class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public double Point;
+
+
+
+    public Student(string name, string surname, string group, double point)
+    {
+        Name = name;
+        Surname = surname;
+        Group = group;
+        Point = point;
+    }
+
+    public string FullName()
+    {
+        return $"{Name} {Surname}";
+
+    }
+
+    public string Info()
+    {
+        return $"{FullName()} {Point} {Group}";
+    }
+
+
+    public bool IsGraduated()
+    {
+       if(Point >= 80)
+        {
+            return true;
+        }
+        else
+        {
+            return false ;
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
